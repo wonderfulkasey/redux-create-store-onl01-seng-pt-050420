13074e4cf1fe93e21c4c4b84de81fa7fd5ad1443
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-08 13:16:57 +0000

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,5 +1,23 @@
 let state;
 
+function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  };
+
+  return {
+    dispatch,
+    getState
+  };
+};
+
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
     case 'INCREASE_COUNT':
